bc49689f26eca3e3184a0de44564b50a5b4eae2d
pmor10 <paresamorton@gmail.com>
2021-03-13 00:27:09 -0800

    updated date format

--- CovidTracker/crud/tracker.py
+++ CovidTracker/crud/tracker.py
@@ -1,12 +1,13 @@
 from CovidTracker.models.model import SymptomTracker
 from CovidTracker.connect import db
-from datetime import datetime 
+import datetime
 
 def create_symptom_tracker(user_id, symptom_id, symptom_date=None):
     """Create and return a new symptom tracker"""
 
     if not symptom_date:
-        symptom_date = datetime.today()
+        today = datetime.datetime.today()
+        symptom_date = datetime.datetime.date(today)
     tracker = SymptomTracker(user_id=user_id, symptom_id=symptom_id, symptom_date=symptom_date)
 
     db.session.add(tracker)
@@ -17,10 +18,14 @@ def create_symptom_tracker(user_id, symptom_id, symptom_date=None):
 
 def get_symptom_tracker_user_id_symptoms(user_id):
 
-    return SymptomTracker.query.filter_by(user_id=user_id).all()
+    user_symptoms =  SymptomTracker.query.filter_by(user_id=user_id).all()
+    
+    return user_symptoms
 
 
-def del_symptom_tracker(user_id, symptom_id):
-
-    SymptomTracker.query.filter(SymptomTracker.symptom_id==symptom_id, SymptomTracker.user_id==user_id).delete()
+def del_symptom_tracker(user_id, symptom_id, symptom_date):
+    
+    d = SymptomTracker.query.filter(SymptomTracker.symptom_id==symptom_id, SymptomTracker.user_id==user_id, SymptomTracker.symptom_date==symptom_date).delete()
+    print('D'*7777, user_id, symptom_id, symptom_date, d)
+    db.session.commit()
 
