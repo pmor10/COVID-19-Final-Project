2fb0e8c7f7e8a0f878c0e9ade377574c42f56424
pmor10 <paresamorton@gmail.com>
2021-03-13 00:26:22 -0800

    updated seed to use new module

--- seed_database.py
+++ seed_database.py
@@ -2,13 +2,12 @@ import os
 from random import choice, randint
 from CovidTracker.app import app 
 from CovidTracker.connect import connect_to_db, db 
-from CovidTracker.crud.symptom import create_symptom, get_symptoms, del_symptom_tracker, get_symptom_tracker, get_symptom_by_id
+from CovidTracker.crud.symptom import create_symptom
+from CovidTracker.models.model import recreate_db
+# os.system('sudo -u postgres dropdb postgres')
+# os.system('sudo -u postgres createdb postgres')
 
-os.system('sudo -u postgres dropdb postgres')
-os.system('sudo -u postgres createdb postgres')
-
-connect_to_db(app)
-db.create_all()
+recreate_db(app)
 
 symptoms = ['Chills', 
             'Congestion or runny nose', 
@@ -25,53 +24,3 @@ symptoms = ['Chills',
 
 for s in symptoms:
     create_symptom(s)
-
-
-# create 10 dummy users
-# for n in range(10):
-
-#     username = f'user{n}'
-#     email = f'user{n}@test.com'
-#     password = 'test'
-
-#     crud.create_user(username, email, password)
-
-# import testing_data_load
-# import vaccine_data_load
-
-
-
-print(get_symptoms())
-
-for user, symptom in [(1,1), (1,2), (1,3), (2,1), (2,4)]:
-
-    create_symptom_tracker(user, symptom)
-
-records = get_symptom_tracker(1)
-
-for record in records:
-    print(record.symptom.symptom_name, record.user.username)
-
-d = del_symptom_tracker(1, 1)
-
-records1 = get_symptom_tracker(1)
-for record in records1:
-    print(record.symptom.symptom_name, record.user.username)
-
-s1 = get_symptom_by_id(1)
-print(s1)
-
-# u1 = crud.upd_symptom_by_id(1, 'Hairloss')
-# print(crud.get_symptom_by_id(1))
-
-
-# crud.create_user('pari', 'pari_morton@email.com', 'CuddlesTheCatIsCool')
-
-# pari = crud.get_user_by_email('pari_morton@email.com')
-# print(pari)
-# crud.upd_user_password(11, 'CuddlesTheCatIsCool', 'ButJasmineIsTheBestest')
-
-# pari = crud.get_user_by_id(11)
-# print(pari)
-
-# print(crud._get_user_password(11))
