ceb53d367a400123a6b4be141e2755b48fe45038
pmor10 <paresamorton@gmail.com>
2021-03-13 00:27:44 -0800

    jsonified data from html button

--- CovidTracker/views/symptom.py
+++ CovidTracker/views/symptom.py
@@ -1,7 +1,7 @@
 from CovidTracker.app import app 
 from CovidTracker.crud.symptom import get_symptoms
 from CovidTracker.crud.tracker import create_symptom_tracker, get_symptom_tracker_user_id_symptoms, del_symptom_tracker
-
+import json 
 import datetime 
 
 from flask import (render_template, request, session, flash, jsonify)
@@ -9,7 +9,6 @@ from flask import (render_template, request, session, flash, jsonify)
 @app.route('/symptoms')
 def symptom_tracker():
     symptoms = get_symptoms()
-
     two_col_symptoms = tuple(zip(symptoms[1::2], symptoms[::2]))
 
     return render_template('symptoms.html', symptoms=symptoms, two_col_symptoms=two_col_symptoms)
@@ -26,7 +25,7 @@ def add_symptoms():
         
         symptom_count = 0
         for symptom in user_symptoms:
-            if datetime.datetime.date(today) == datetime.datetime.date(symptom.symptom_date):
+            if datetime.datetime.date(today) == symptom.symptom_date:
                 symptom_count += 1
         
         if symptom_count >= 3:
@@ -57,9 +56,15 @@ def delete_symptom():
     """ Delete testing location from the database """
 
     user_id = session.get('user_id', None)
-    symptom_id = request.form.get('symptom_id')
-    del_symptom_tracker(user_id, symptom_id)
+    data = request.get_data();
 
+    myjson = json.loads(data.decode('utf-8').replace("'", '"'))
+    
+    symptom_id = myjson['symptom_id']
+    symptom_date = myjson['symptom_date']
+    
+    del_symptom_tracker(user_id, symptom_id, symptom_date)
+    
     flash("Location removed!")
 
     return jsonify("Success!")
